9e11f4014417c5a5071a7ac9e919e84e3ffbe091
laurazig <lhzig@protonmail.com>
2022-10-15 13:04:30 +0200

    reinstalled FE create-react-app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Leipzig Christmas Market</h1>
+      </header>
+      <main>
+        <h2>Sellers</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
